2f25c1851d419c37292edcef7d800cdc5669d494
Alex <alxsandin@gmail.com>
2024-11-30 00:15:26 +0300

    wordle done

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,31 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] dictionary = new String[]{
-                "точка",
-                "книга",
-                "комар",
-                "слово",
-                "почка",
-                "дочка",
-                "ручка",
-                "мячик",
-                "пачка",
-                "радий",
-                "весна",
-                "осень",
-                "нолик"
-        };
-        int secretWordIndex = new Random().nextInt(dictionary.length);
-        String secretWord = dictionary[secretWordIndex].toLowerCase();
-        int maxAttempts = 10;
-        System.out.println("Добро пожаловать в Wordle!");
-        System.out.println("У вас будет " + maxAttempts + " попыток, что бы отгадать секретное слово из 5-и букв");
+        int maxAttempts = 5;
+        WordleGame game = new WordleGame();
+
+        System.out.println(game.getGreetings());
+        System.out.println(game.getRules());
+
         Scanner scanner = new Scanner(System.in);
         for (int attempt = 1; attempt <= maxAttempts; attempt++) {
             System.out.print("Попытка " + attempt + ": ");
             String userInput = scanner.nextLine().toLowerCase();
-            if (secretWord.equals(userInput)) {
-                System.out.println("Поздравляем! Вы отгадали секретное слово!");
+
+            if (game.isCorrect(userInput)) {
+                System.out.println(game.getCongratulations());
                 return;
             } else {
-                String feedback = getFeedback(secretWord, userInput);
+                String feedback = game.getFeedback(userInput);
                 System.out.println("Подсказка: " + feedback);
             }
         }
-        System.out.println("Вы проиграли. Секретное слово: " + secretWord);
+        System.out.println(game.getFailMessage());
     }
+}
 
-    static String getFeedback(String secretWord, String guess) {
-        if (secretWord.length() != guess.length()) {
-            return "Пожалуйста введите слово из 5-и букв";
-        }
-        String feedback = "";
-        for (int i = 0; i < secretWord.length(); i++) {
-            char currentSymbol = guess.charAt(i);
-            if (secretWord.charAt(i) == currentSymbol) {
-                feedback += currentSymbol;
-            } else if (secretWord.contains(String.valueOf(currentSymbol))) {
-                feedback += "?";
-            } else {
-                feedback += "_";
-            }
-        }
-        return feedback;
-    }
-    /*
+/*
 Загадать секретное слово
 Повторять пока есть попытки
     Запросить у игрока вариант
@@ -66,4 +36,22 @@ public class Main {
     ? - буква не на своей позиции
     Вывести букву, если она на правильной позиции
 */
-}
+
+
+// комар
+
+// точка
+// _о_??
+
+// кочка
+// ко_??
+
+// кочан
+// ко_а_
+
+// комар
+//
+
+
+// _ - отсутствует в секретном слове
+// ? - буква не на своей позиции
